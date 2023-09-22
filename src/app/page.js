ea4ddc7302d217eaacacd1495878f27dc18ef561
Iyanu456 <oyerindei13@gmail.com>
2023-09-22 09:33:17 +0100

    working on landing page responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,42 @@
 import Navigation from "./components/Navigation";
 import Image from "next/image";
+import Link from 'next/link';
 import "./main.css";
-import Title from './hackathon-assets/Title.svg'
-import TitleMobile from './hackathon-assets/Title-mobile.svg'
-import VrImage from './hackathon-assets/man-wearing-vr-googles.png' 
+import Title from "./hackathon-assets/Title.svg";
+import TitleMobile from "./hackathon-assets/Title-mobile.svg";
+import VrImage from "./hackathon-assets/man-wearing-vr-googles.png";
 export default function Home() {
 	return (
 		<div className="main-container">
 			<Navigation />
-			<div className="caption">
-				<h4><b><i>Igniting  a Revolution in HR Innovation</i></b></h4>
+			<div
+				className="caption"
+				style={{
+					paddingTop: "1.5em",
+					borderTop: "1px solid rgba(124, 124, 124, 0.346)",
+				}}>
+				<h4>
+					<b>
+						<i>Igniting a Revolution in HR Innovation</i>
+					</b>
+				</h4>
 			</div>
 			<div className="hero-section">
-
 				<section>
-					<Image className="web" src={Title} alt="Title"/>
-					<Image className="mobile" src={TitleMobile} alt="Title"/>
-					<p>Participate in getlinked tech Hackathon 2023 stand <br />a chance to win a Big prize </p>
-					<button className="btn-gradient">Register</button>
+					<Image className="web" src={Title} alt="Title" />
+					<Image className="mobile" src={TitleMobile} alt="Title" />
+					<p>
+						Participate in getlinked tech Hackathon 2023 stand a
+						chance to win a Big prize
+					</p>
+					<Link href="/contact-page">
+						<button className="btn-gradient">Register</button>
+					</Link>
+					<div className="counter">00<span>H</span> 00<span>M</span> 00<span>S</span></div>
 				</section>
 
 				<section>
-					<Image src={VrImage} alt="VrImage"/>
+					<Image src={VrImage} alt="VrImage" />
 				</section>
 			</div>
 		</div>
