4100e682257a1598398c1882cf7f048bd5916cca
Iyanu456 <oyerindei13@gmail.com>
2023-09-22 12:12:08 +0100

    landing page ui improvements

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import Navigation from "./components/Navigation";
 import Image from "next/image";
-import Link from 'next/link';
+import Link from "next/link";
 import "./main.css";
 import Title from "./hackathon-assets/Title.svg";
-import Caption from './hackathon-assets/caption.svg';
+import Caption from "./hackathon-assets/caption.svg";
 import TitleMobile from "./hackathon-assets/Title-mobile.svg";
 import VrImage from "./hackathon-assets/man-wearing-vr-googles.svg";
-import Star from './hackathon-assets/icons/star.svg';
+import Star from "./hackathon-assets/icons/star.svg";
+import Idea from "./hackathon-assets/The-big-idea.svg";
 export default function Home() {
 	return (
 		<div className="main-container">
@@ -22,7 +23,6 @@ export default function Home() {
 			<Image src={Star} id="star-1" alt="starr" />
 			<div className="hero-section">
 				<section>
-					
 					<Image className="web" src={Title} alt="Title" />
 					<Image className="mobile" src={TitleMobile} alt="Title" />
 					<p>
@@ -32,12 +32,31 @@ export default function Home() {
 					<Link href="/contact-page">
 						<button className="btn-gradient">Register</button>
 					</Link>
-					<div className="counter">00<span>H</span> 00<span>M</span> 00<span>S</span></div>
+					<div className="counter">
+						00<span>H</span> 00<span>M</span> 00<span>S</span>
+					</div>
 				</section>
 
 				<section>
 					<Image src={VrImage} alt="VrImage" />
 				</section>
+			</div>
+			<div className="hackathon-grp">
+				<div>
+					<Image src={Idea} alt="Idea" />
+				</div>
+				<div>
+					<h4 className="heading">Introduction to getlinked <span style={{ color: "#d434ff" }}>tech Hackathon 1.0</span></h4>
+					<p className="content">
+						Our tech hackathon is a melting pot of visionaries, and
+						its purpose is as clear as day: to shape the future.
+						Whether you're a coding genius, a design maverick, or a
+						concept wizard, you'll have the chance to transform your
+						ideas into reality. Solving real-world problems, pushing
+						the boundaries of technology, and creating solutions
+						that can change the world, that's what we're all about!
+					</p>
+				</div>
 			</div>
 		</div>
 	);
