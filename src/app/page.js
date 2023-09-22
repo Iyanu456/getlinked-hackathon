8642745e22f330e1ae2ac6751b1651342cbac5d5
Iyanu456 <oyerindei13@gmail.com>
2023-09-22 12:19:16 +0100

    landing page ui improvements

--- src/app/page.js
+++ src/app/page.js
@@ -46,15 +46,16 @@ export default function Home() {
 					<Image src={Idea} alt="Idea" />
 				</div>
 				<div>
-					<h4 className="heading">Introduction to getlinked <span style={{ color: "#d434ff" }}>tech Hackathon 1.0</span></h4>
+					<h4 className="heading">
+						Introduction to getlinked{" "}
+						<span style={{ color: "#d434ff" }}>
+							tech Hackathon 1.0
+						</span>
+					</h4>
 					<p className="content">
-						Our tech hackathon is a melting pot of visionaries, and
-						its purpose is as clear as day: to shape the future.
-						Whether you're a coding genius, a design maverick, or a
-						concept wizard, you'll have the chance to transform your
-						ideas into reality. Solving real-world problems, pushing
-						the boundaries of technology, and creating solutions
-						that can change the world, that's what we're all about!
+						{
+							"Our tech hackathon is a melting pot of visionaries, and its purpose is as clear as day: to shape the future. Whether you're a coding genius, a design maverick, or a concept wizard, you'll have the chance to transform your ideas into reality. Solving real-world problems, pushing the boundaries of technology, and creating solutions that can change the world, that's what we're all about!"
+						}
 					</p>
 				</div>
 			</div>
